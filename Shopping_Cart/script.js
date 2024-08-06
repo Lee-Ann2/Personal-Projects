fe1ccff424006843a59628909edfc59a88d4b703
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-08-06 23:28:23 +0200

    Waiting to add some Javascript.

---
+++ Shopping_Cart/script.js
@@ -0,0 +1,46 @@
+const menu = [{
+    image:"FreeImageKit.com_Img-size(240x240)px (2).jpg",
+    name:"Creamy Garlic Chicken",
+    price:6075
+},
+{
+    image:"hot-and-sour.jpeg",
+    name:"Hot And Sour Dumplings",
+    price:6000
+},
+{
+  image:"FreeImageKit.com_Img-size(240x240)px (4).jpg",
+  name:"Roast Beef",
+  price:12050  
+},
+{
+    image:"FreeImageKit.com_Img-size(240x240)px (3).jpg",
+    name:"Mushroom Soup",
+    price:4000
+},
+{
+    image:"creamy-garlic-pasta.jpeg",
+    name:"Creamy Garlic Pasta",
+    price:8000
+},
+{
+    image:"FreeImageKit.com_Img-size(240x240)px (5).jpg",
+    name:"Triple Decker Pizza",
+    price:8000
+},
+{
+    image:"Steak.jpeg",
+    name:"Steak",
+    price:9025
+},
+{
+    image:"creamy-chicken-pasta.jpeg",
+    name:"Creamy Chicken Pasta",
+    price:12050
+},
+{
+    image:"garlic-bread.jpeg",
+    name:"Garlic Bread",
+    price:3000
+}
+]
