a33a2f205174498e18e0c5a3634affe6e7000b89
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-09-06 02:47:22 +0200

    Added a few comments in the js file

--- EnterAge/script.js
+++ EnterAge/script.js
@@ -1,10 +1,10 @@
 const myText = document.getElementById('myText');
 const btn = document.getElementById('btn');
-let age;
+let age; // always use let if a var will change 
 
 btn.onclick = function() {
-    age = myText.value;
-    age = Number(age);
+    age = myText.value; // Always accept value when using input.
+    age = Number(age); // convert a string to a number
     if(age < 18) {
         alert('Get the hell outta here lil hommie');
     } else if(age == 18) {
